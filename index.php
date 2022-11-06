77f76e24e7c7a7e01453919b6a0be0ef854efa0b
EduRafa1 <youcodelove1@gmail.com>
2022-11-06 14:23:46 -0300

    'comentario'

--- index.php
+++ index.php
@@ -81,33 +81,4 @@
     }else{
         http_response_code(204);
     }
-
-    /*
-    if (isset($_GET['url'])) {    
-        $url = explode('/', $_GET['url']);
-        if ($url[0] === 'api') {
-            array_shift($url);                  
-            $service = 'App\Services\\'.ucfirst($url[0]).'Service';
-            array_shift($url);
-            $method = strtolower($_SERVER['REQUEST_METHOD']);
-            try {
-                echo $service;
-                echo $method;
-                $response = call_user_func_array(array(new $service, $method), $url); 
-                echo $response;
-
-                http_response_code(200);
-                echo json_encode(array('status'=>'success','data'=>$response));
-                exit;
-            } catch (\Exception $e) {
-                http_response_code(404);
-                echo json_encode(array('status'=>'error','data'=>$e->getMessage()),JSON_UNESCAPED_UNICODE);
-                exit;
-            } 
-       
-       }
-
-    }  
-       */
-     
  ?>
